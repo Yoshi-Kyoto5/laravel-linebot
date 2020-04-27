691176453e2e8f6392c48abbd3e165a9fffe0c7a
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-04-28 08:51:27 +0900

    change miss code of character

--- laravel/app/Http/Controllers/LineBotController.php
+++ laravel/app/Http/Controllers/LineBotController.php
@@ -19,11 +19,11 @@ class LineBotController extends Controller
         Log::debug($request->header());
         Log::debug($request->input());
 
-        $httpClient = new CualHTTPClient(env('LINE_ACCESS_TOKEN'));
+        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
         $lineBot = new LINEBot($httpClient,['channelSecret' => env('LINE_CHANNEL_SECRET')]);
 
         $signature = $request->header('x-line-signature');
-        if (!$lineBot->validatesSignature($request->getContent(), $signature)){
+        if (!$lineBot->validateSignature($request->getContent(), $signature)) {
             abort(400, 'Invalid signature');
         }
 
