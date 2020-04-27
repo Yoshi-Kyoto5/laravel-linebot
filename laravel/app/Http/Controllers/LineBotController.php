2fb0ac10169699bc212c86903709ee64507e6d48
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-04-27 15:17:54 +0900

    make instance of LineBot

--- laravel/app/Http/Controllers/LineBotController.php
+++ laravel/app/Http/Controllers/LineBotController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Log;
+
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class LineBotController extends Controller
 {
@@ -15,5 +17,8 @@ class LineBotController extends Controller
     public function parrot(Request $request){
         Log::debug($request->header());
         Log::debug($request->input());
+
+        $httpClient = new CualHTTPClient(env('LINE_ACCESS_TOKEN'));
+        $lineBot = new LINEBot($httpClient,['channelSecret' => env('LINE_CHANNEL_SECRET')]);
     }
 }
