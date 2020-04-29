04a5c84442570e05f2ee2e429b1c47a75aac0108
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-04-29 20:20:27 +0900

    set searching results in property

--- laravel/app/Services/RestaurantBubbleBuilder.php
+++ laravel/app/Services/RestaurantBubbleBuilder.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Arr;
+
 use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder;
 
 class RestaurantBubbleBuilder implements ContainerBuilder
@@ -20,5 +22,19 @@ class RestaurantBubbleBuilder implements ContainerBuilder
   public static function builder(): RestaurantBubbleBuilder
   {
     return new self();
+  }
+
+  public function setContents(array $restaurant): void
+  {
+    $this->imageUrl = Arr::get($restaurant, 'image_url.shop_image1', null);
+    $this->name = Arr::get($restaurant, 'name', null);
+    $this->closestStation = Arr::get($restaurant, 'access.station', null);
+    $this->minutesByFoot = Arr::get($restaurant, 'access.walk', null);
+    $this->category = Arr::get($restaurant, 'category', null);
+    $this->budget = Arr::get($restaurant, 'budget', null);
+    $this->latitude = Arr::get($restaurant, 'latitude', null);
+    $this->longitude = Arr::get($restaurant, 'longitude', null);
+    $this->phoneNumber = Arr::get($restaurant, 'tel', null);
+    $this->restaurantUrl = Arr::get($restaurant, 'url', null);
   }
 }
