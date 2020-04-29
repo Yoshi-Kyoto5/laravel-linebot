5e2833c784a3bf9e7efae3a8e5136bbbeed4f8f8
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-04-29 20:00:08 +0900

    make various property

--- laravel/app/Services/RestaurantBubbleBuilder.php
+++ laravel/app/Services/RestaurantBubbleBuilder.php
@@ -6,4 +6,14 @@ use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder;
 
 class RestaurantBubbleBuilder implements ContainerBuilder
 {
+  private $imageUrl;
+  private $name;
+  private $closestStation;
+  private $minutesByFoot;
+  private $category;
+  private $budget;
+  private $latitude;
+  private $longitude;
+  private $phoneNumber;
+  private $restaurantUrl;
 }
